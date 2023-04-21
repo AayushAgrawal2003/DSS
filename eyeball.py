3db41a525761872875aeb7a5ff8b5200e8864dbb
Aahana Hegde <94188928+lizardkun@users.noreply.github.com>
2023-04-21 23:46:10 +0530

    Update eyeball.py

--- eyeball.py
+++ eyeball.py
@@ -1,23 +1,24 @@
 import rospy
 import cv2
 from sensor_msgs.msg import Image
-from sensor_msgs.msg import CameraInfo
 from gaze_tracking import GazeTracking
 from cv_bridge import CvBridge, CvBridgeError
+
+rospy.init_node('zed_converter',anonymous=True)
 gaze = GazeTracking()
 class Zed_Video:
     def __init__(self):
         # subscribing to the image data from the virat camera
         self.image_sub = rospy.Subscriber(
-            "zed2/zed_node/rgb/image_raw", Image, self.callback
+            "zed2i/zed_node/rgb_raw/image_raw_color", Image, self.callback
         )
+        self.bridge = CvBridge()
+        
     def callback(self, data):
-
-        bridge = CvBridge()
 
         try:
             # using CVbridge to convert a ros image to a cv2 image
-            cv_image = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
+            cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
             
             while True:
                 _, frame = cv_image
@@ -25,7 +26,6 @@ class Zed_Video:
 
         except CvBridgeError as e:
             rospy.logerr(e)
-
-gaze = GazeTracking()
-webcam = cv2.VideoCapture(0)
-Zed_Video()
+            
+vd=Zed_Video()
+rospy.spin()
